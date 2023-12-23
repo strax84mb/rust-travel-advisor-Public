9a25160000f4ebd2ca0e21e4e115ae6909abfcfd
Strahinja Dobrijevic <s.dobrijevic@levi9.com>
2023-12-23 15:10:48 +0100

    Proof Of Concept for actix responses and error

--- travel_advisor/src/api/hello.rs
+++ travel_advisor/src/api/hello.rs
@@ -10,6 +10,7 @@ use actix_web::{
         Data,
     },
 };
+use serde::Serialize;
 
 use crate::services::traits::CityService;
 use super::{
@@ -29,6 +30,7 @@ pub fn init(cfg: &mut web::ServiceConfig) {
                     .guard(actix_web::guard::Get())
                     .to(take_test_for_a_ride)
             ).service(test_save_city)
+            .service(hello_with_json_payloads)
     );
 }
 
@@ -59,5 +61,80 @@ async fn test_save_city(
     match city_service.new(name.to_string()) {
         Ok(city) => respond_ok(Some(CityDto::from_model(&city))),
         Err(err) => resolve_error(err, Some("failed to save city"))
+    }
+}
+
+#[derive(Serialize)]
+struct HelloTestPayload {
+    id: i32,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    name: Option<String>,
+    active: bool,
+}
+
+#[derive(Serialize, Debug, derive_more::Display, derive_more::Error)]
+enum TestErrorEnum {
+    #[display(fmt="internal")]
+    Internal(TestError),
+
+    #[display(fmt="internal")]
+    NotFound(TestError),
+}
+
+#[derive(Serialize, Debug, derive_more::Error, derive_more::Display)]
+#[display(fmt = "error")]
+struct TestError {
+    code: String,
+    description: String,
+}
+
+impl actix_web::ResponseError for TestErrorEnum {
+
+    fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
+        let (mut builder, payload) = match self {
+            Self::Internal(p) => (HttpResponse::InternalServerError(), p),
+            Self::NotFound(p) => (HttpResponse::NotFound(), p),
+        };
+        match serde_json::to_string(payload) {
+            Ok(str_payload) => builder.body(str_payload),
+            Err(err) => HttpResponse::InternalServerError().body(
+                format!("failed to serialize payload. Cause: {}", err.to_string())
+            )
+        }
+    }
+
+    fn status_code(&self) -> actix_web::http::StatusCode {
+        match self {
+            Self::Internal(_) => actix_web::http::StatusCode::INTERNAL_SERVER_ERROR,
+            Self::NotFound(_) => actix_web::http::StatusCode::NOT_FOUND,
+        }
+    }
+
+}
+
+#[get("/test-payloads/{type}")]
+async fn hello_with_json_payloads(
+    payload_type: web::Path<String>,
+) -> Result<web::Json<HelloTestPayload>, TestErrorEnum> {
+    match payload_type.as_str() {
+        "test" => Ok(web::Json(HelloTestPayload {
+            id: 1,
+            name: None,
+            active: false,
+        })),
+        "named" => Ok(web::Json(HelloTestPayload {
+            id: 2,
+            name: Some("Strale".to_string()),
+            active: true,
+        })),
+        "not-found" => Err(TestErrorEnum::NotFound(TestError {
+            code: "NOT_FOUND".to_string(),
+            description: "Did not find what I need!!!".to_string(),
+        })),
+        _ => Err(TestErrorEnum::Internal(TestError {
+                code: "TEST_CODE".to_string(),
+                description: "Some text to see".to_string(),
+            }
+        )),
     }
 }
